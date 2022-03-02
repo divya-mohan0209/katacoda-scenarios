1e896fd72f5bc8e73863a031dc1f7d916fedc729
divya-mohan0209 <divya.mohan0209@gmail.com>
2022-03-02 18:54:22 +0530

    Fifth commit

--- kubewarden-scenario/install-kubernetes.sh
+++ kubewarden-scenario/install-kubernetes.sh
@@ -3,7 +3,8 @@ echo "step1"
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 echo "kubeadm install"
 sudo apt update -y
-sudo apt -y install vim git curl wget kubelet=1.22.0-00 kubeadm=1.22.0-00 kubectl=1.22.0-00
+sudo apt -y install vim git curl wget kubelet=1.23.0-00 kubeadm=1.23.0-00 kubectl=1.23.0-00
+sudo apt-mark hold kubelet kubeadm kubectl
 echo "memory swapoff"
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 sudo swapoff -a
@@ -34,14 +35,11 @@ containerd config default>/etc/containerd/config.toml
 sudo systemctl restart containerd
 sudo systemctl enable containerd
 sudo systemctl enable kubelet
-sudo kubeadm config images pull --cri-socket /run/containerd/containerd.sock --kubernetes-version v1.22.0
-sudo kubeadm init   --pod-network-cidr=192.168.0.0/16   --upload-certs --kubernetes-version=v1.22.0  --control-plane-endpoint=host01 --ignore-preflight-errors=Mem  --cri-socket /run/containerd/containerd.sock
+sudo kubeadm config images pull --cri-socket /run/containerd/containerd.sock --kubernetes-version v1.23.0
+sudo kubeadm init   --pod-network-cidr=10.244.0.0/16   --upload-certs --kubernetes-version=v1.23.0  --control-plane-endpoint=host01 --ignore-preflight-errors=Mem  --cri-socket /run/containerd/containerd.sock
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 export KUBECONFIG=/etc/kubernetes/admin.conf
 kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
 kubectl taint node host01 node-role.kubernetes.io/master:NoSchedule-
-wget https://get.helm.sh/helm-v3.7.2-linux-amd64.tar.gz
-tar -xvf helm-v3.7.2-linux-amd64.tar.gz
-mv linux-amd64/helm /usr/local/bin/
